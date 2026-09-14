ecf08f7ce9fb2013d6cd7b8a28e00dfb0365e0ae
Musaib Asif <musaibasif1122@gmail.com>
2026-09-14 22:50:59 +0500

    Done

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const temp = document.querySelector("#temp-value");
+const tempC = document.querySelector("#temp-value-c");
+const tempF = document.querySelector("#temp-value-f");
 const humi = document.querySelector("#humi-value");
+const feelsLikeC = document.querySelector("#feels-like-c");
+const feelsLikeF = document.querySelector("#feels-like-f");
+const rainChances = document.querySelector("#rain-chances");
+const windSpeed = document.querySelector("#wind-speed");
+const city = document.querySelector("#city");
+const table = document.querySelector("#table");
 
 async function wheather(event) {
   event.preventDefault();
 
+  
+  // table.innerHTML = "Loading..."
   const input = document.querySelector("#input-value").value;
 
   const response = await axios(
@@ -11,6 +20,20 @@ async function wheather(event) {
   );
   console.log(response);
 
-  temp.innerHTML = response.data.current.temp_c;
-  temp.innerHTML = response.data.current.humidity;
+  tempC.innerHTML = response.data.current.temp_c + "°C";
+  tempF.innerHTML = response.data.current.temp_f + "°F";
+  humi.innerHTML = response.data.current.humidity + "%";
+  feelsLikeC.innerHTML = response.data.current.feelslike_c + "°C";
+  feelsLikeF.innerHTML = response.data.current.feelslike_f + "°F";
+  rainChances.innerHTML = response.data.current.chance_of_rain + "%";
+  windSpeed.innerHTML = response.data.current.wind_mph + "mph";
+  city.innerHTML = response.data.location.name;
 }
+// async function show() {
+//   const response = await axios(
+//     `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=karachi`,
+//   );
+//   console.log(response);
+// }
+
+// show();

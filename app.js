151c3d9c892a69c39e48a327664978dc7186df55
musaibasif12 <musaibasif1122@gmail.com>
2026-09-17 11:19:04 +0500

    Error Handling

--- app.js
+++ app.js
@@ -7,32 +7,31 @@ const rainChances = document.querySelector("#rain-chances");
 const windSpeed = document.querySelector("#wind-speed");
 const city = document.querySelector("#city");
 const table = document.querySelector("#table");
-const dT = document.querySelector("#current-date")
+const dT = document.querySelector("#current-date");
+
 async function wheather(event) {
   event.preventDefault();
 
-  
-  // table.innerHTML = "Loading..."
-  const input = document.querySelector("#input-value").value;
+  try {
+    const input = document.querySelector("#input-value").value;
+    const response = await axios(
+      `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${input}`,
+    );
 
-  const response = await axios(
-    `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${input}`,
-  );
-  console.log(response);
-
-  tempC.innerHTML = response.data.current.temp_c + "°C";
-  tempF.innerHTML = response.data.current.temp_f + "°F";
-  humi.innerHTML = response.data.current.humidity + "%";
-  feelsLikeC.innerHTML = response.data.current.feelslike_c + "°C";
-  feelsLikeF.innerHTML = response.data.current.feelslike_f + "°F";
-  rainChances.innerHTML = response.data.current.chance_of_rain + "%";
-  windSpeed.innerHTML = response.data.current.wind_mph + "mph";
-  city.innerHTML = response.data.location.name;
+    tempC.innerHTML = response.data.current.temp_c + "°C";
+    tempF.innerHTML = response.data.current.temp_f + "°F";
+    humi.innerHTML = response.data.current.humidity + "%";
+    feelsLikeC.innerHTML = response.data.current.feelslike_c + "°C";
+    feelsLikeF.innerHTML = response.data.current.feelslike_f + "°F";
+    rainChances.innerHTML = response.data.current.chance_of_rain + "%";
+    windSpeed.innerHTML = response.data.current.wind_mph + "mph";
+    city.innerHTML = response.data.location.name;
+  } catch (error) {
+    table.innerHTML = error.response.data.error.message;
+  }
 }
 
 async function showWheather() {
- 
-
   const response = await axios(
     `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=karachi`,
   );
